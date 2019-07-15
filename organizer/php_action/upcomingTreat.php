7772f4c704f3069a236b94f5ae029bb78f904dbb
sajeeha1997 <52922881+sajeeha1997@users.noreply.github.com>
2019-07-15 20:34:18 +0530

    Create upcomingTreat.php

---
+++ organizer/php_action/upcomingTreat.php
@@ -0,0 +1,53 @@
+<?php
+            require_once('connection.php');  
+                $dbobj=new dbconnect();
+	            $con=$dbobj->getcon();
+	   
+			//Select Query to fetch all the records in a table
+			$query = "SELECT * FROM treat where treatStatus ='confirmed'  OR treatStatus ='pending' ";
+		
+			$exec = mysqli_query($con,$query);
+			if($exec){
+				$nor =  mysqli_num_rows($exec);//Get the number of matching records.
+				if($nor>0)
+				{	
+					echo "<table id='tableUpcomingTreat' border=1 width=100%>
+						<tr>
+							<th height=50px >Treat ID</th>
+                            <th>Date</th>
+                            <th>Time</th>                    
+                            <th>Venue</th>  
+                            <th>Note </th>
+                            <th>Status</th>
+							<th>Action</th>
+							
+						</tr>";
+					while($record = mysqli_fetch_array($exec))
+					{
+						$traetID = $record['treat_Id'];
+						echo "<tr>
+							<td>".$record["treat_Id"]."</td>
+							<td>".$record["treatDate"]."</td>
+                            <td>".$record["treatTime"]."</td>
+                            <td>".$record["venue"]."</td>
+                            <td>".$record["notes"]."</td>
+                            <td>".$record["treatStatus"]."</td>
+                            <td>"."<button type='button' class='btn_sub' id='btnConfirmed-".$traetID."'onclick='statusChange(this.id)'>Confirmed</button> <button type='button' id='btnCelebrated-".$traetID."' onclick='statusChange(this.id)' >Celebrated</button> <button type='button' id='btnCancelled-".$traetID."' onclick='statusChange(this.id)' >Cancel</button> "."</td>
+                            
+							
+							
+						</tr>";
+					}
+					echo "</table>";
+				}
+				else
+				{
+					echo "No UpComing Treat ! ".mysqli_error($con);
+				}
+			}
+			else{
+				echo " DB could not be Found! ".mysqli_error($con);
+			}
+			
+			$dbobj->close();
+		?>
