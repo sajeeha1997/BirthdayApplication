0ebf1880f346a224cb11c3efffcffe15057767af
sajeeha1997 <52922881+sajeeha1997@users.noreply.github.com>
2019-07-15 20:05:55 +0530

    Create treat.php

---
+++ admin/treat.php
@@ -0,0 +1,33 @@
+<html>
+<head>
+    <title>Dashbord Admin</title>
+    <style >
+        ul{
+            list-style-type: none;
+            margin: 0;
+            padding: 0;
+        }
+        li {
+            display: inline;
+            float: left;
+            width: 60px;
+            border: 1px solid ;
+            width: 18%;
+        }
+    </style>
+</head>
+<body>
+    <h1>Admin Dashbord</h1>
+    <hr>
+    <ul>
+        <li ><a href="adminDashbord.php">Dashbord</a></li>
+        <li><a href="organizingTeam.php">Organizing Team</a></li>
+        <li><a href="users.php">Users</a> </li>
+        <li><a href="treat.php">treat</a></li>
+        <li><a href="../login.php">Logout</a></li>
+    </ul>
+    <br>
+    <hr><br>
+       <h3>Treat</h3>
+</body>
+</html>
